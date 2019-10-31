0dabb4610598d4bfe52d5767cbdc84a5b9d1cd09
Michael Dorst <m@mdorst.net>
2019-10-30 17:13:59 -0700

    Game starts paused
    Add WASD and vim keys for player control
    
    Redraw snake more efficiently
    
    Add documenting comments

--- draw.js
+++ draw.js
@@ -4,16 +4,23 @@ const unitSize = 20
 const backgroundColor = "#111"
 const snakeColor = "#aaa"
 
+// Draws an individual unit of the snake in the color specified
 const drawUnit = (unit, color) => {
   ctx.fillStyle = color
   ctx.fillRect(unit.x * unitSize, (29 - unit.y) * unitSize, unitSize, unitSize)
 }
 
-const drawSnake = (snake) => {
+// Draws the new head and erases the old tail. The middle of the snake does not change between
+// frames, so drawing the snake can be simplified this way.
+const drawUpdatedSnake = (snake) => {
   // Erase the tail by drawing it in background color
   drawUnit(snake[0], backgroundColor)
-  for (let i = 1; i < snake.length; i++)
-  {
+  drawUnit(snake[snake.length - 1], snakeColor)
+}
+
+// Called only once at the beginning to draw the full snake body
+const drawFullSnake = (snake) => {
+  for (let i = 1; i < snake.length; i++) {
     drawUnit(snake[i], snakeColor)
   }
 }
--- game.js
+++ game.js
@@ -5,6 +5,11 @@ const ctx = canvas.getContext('2d')
 canvas.width = 600
 canvas.height = 600
 
+// This structure represents the snake as a series of units, each with a position and a direction
+// they are travelling in.
+// Each frame this structure will be updated, moving each unit in its respective direction, and then
+// updating each unit's direction to follow the unit in front of it. The head unit direction will be
+// set freely based on user input.
 let snake = [
   { x: 13, y: 15, dir: 'east', tail: true },
   { x: 14, y: 15, dir: 'east' },
@@ -12,8 +17,12 @@ let snake = [
   { x: 16, y: 15, dir: 'east' }
 ]
 
+// Draw the full snake once at the start of the game
+drawFullSnake(snake)
+
+// Called once per frame
 const update = () => {
-  drawSnake(snake)
+  // Move each unit in its respective direction
   for (let i = 0; i < snake.length; i++) {
     if (snake[i].dir === 'north')
       snake[i].y++
@@ -23,29 +32,37 @@ const update = () => {
       snake[i].y--
     if (snake[i].dir === 'west')
       snake[i].x--
-
     // Change unit direction to follow the next unit
     if (i < snake.length - 1) {
       snake[i].dir = snake[i + 1].dir
     }
   }
+  // Redraw snake to reflect updates
+  drawUpdatedSnake(snake)
 }
 
-window.setInterval(() => {
-  update()
-}, 150)
+const startGame = () => {
+  document.getElementById('message').style.display = "none"
+  // Update loop - called once every 150 milliseconds
+  window.setInterval(() => {
+    update()
+  }, 150)
+}
 
+// Set the head unit's direction based on keyboard input (arrow keys)
 document.addEventListener('keydown', event => {
-  if (event.key == 'ArrowUp')
+  if (event.key === 'ArrowUp' || event.key === 'w' || event.key === 'k')
     if (snake[snake.length - 1].dir != 'south')
       snake[snake.length - 1].dir = 'north'
-  if (event.key === 'ArrowDown')
+  if (event.key === 'ArrowDown' || event.key === 's' || event.key === 'j')
     if (snake[snake.length - 1].dir != 'north')
       snake[snake.length - 1].dir = 'south'
-  if (event.key == 'ArrowRight')
+  if (event.key == 'ArrowRight' || event.key === 'd' || event.key === 'l')
     if (snake[snake.length - 1].dir != 'west')
       snake[snake.length - 1].dir = 'east'
-  if (event.key === 'ArrowLeft')
+  if (event.key === 'ArrowLeft' || event.key === 'a' || event.key === 'h')
     if (snake[snake.length - 1].dir != 'east')
       snake[snake.length - 1].dir = 'west'
+  if (event.key === ' ')
+    startGame()
 })
