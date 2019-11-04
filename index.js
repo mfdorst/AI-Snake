35de078ba9ed3a675f263241d310bba97b4f8fcb
Michael Dorst <m@mdorst.net>
2019-11-03 22:30:09 -0800

    Use bootstrap

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Entry point. Sets initial state, and listens for keyboard input.
 document.addEventListener('keydown', (() => {
   const ctx = (() => {
-    const canvas = document.querySelector('.canvas')
+    const canvas = document.querySelector('canvas')
     const ctx = canvas.getContext('2d')
     canvas.width = 600
     canvas.height = 600
